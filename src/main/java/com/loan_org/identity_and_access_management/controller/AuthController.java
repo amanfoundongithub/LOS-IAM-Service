3ad870a6f0b527be4b834f0ec4d1fda4b913611e
Aman Raj <billygates124356@gmail.com>
2026-05-19 21:36:18 +0530

    added base url mapper

--- src/main/java/com/loan_org/identity_and_access_management/controller/AuthController.java
+++ src/main/java/com/loan_org/identity_and_access_management/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("${info.base_url}")
 public class AuthController {
 
     @Autowired
