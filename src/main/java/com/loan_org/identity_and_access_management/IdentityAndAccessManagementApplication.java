959b0a8d3710f9ce512f7ce434df3613ae294a5d
Aman Raj <billygates124356@gmail.com>
2026-05-19 14:36:13 +0530

    Added intial configuration for IAM

---
+++ src/main/java/com/loan_org/identity_and_access_management/IdentityAndAccessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.loan_org.identity_and_access_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAndAccessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAndAccessManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loan_org/identity_and_access_management/IdentityAndAccessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_org.identity_and_access_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAndAccessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
