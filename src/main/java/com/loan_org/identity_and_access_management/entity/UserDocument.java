1f1bd474fdd288ab17db1a9a6e1e5b86a522ae7d
Aman Raj <billygates124356@gmail.com>
2026-05-24 13:53:56 +0530

    added doc strings for the UserDocument

--- src/main/java/com/loan_org/identity_and_access_management/entity/UserDocument.java
+++ src/main/java/com/loan_org/identity_and_access_management/entity/UserDocument.java
@@ -1,25 +1,43 @@
 package com.loan_org.identity_and_access_management.entity;
 
-import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.Map;
 
 /**
- * MongoDB instance of the entire user entity. Contains all fields
- * related to security and identity management.
+ * Root MongoDB document representing a user account in the
+ * Identity and Access Management (IAM) system.
+ *
+ * <p>Encapsulates identity data, security information,
+ * access-related attributes, and metadata.</p>
  *
  * @author Aman Raj
  */
-@Data
-@Document(collection = "user")
+@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDocument {
 
     @Id
     private String id;
+
+    @Indexed(unique = true)
+    @NotBlank
+    @Email
     private String email;
+
+    @Indexed(unique = true)
+    @NotBlank
     private String username;
+
+    // Status of user's account
     private UserStatus status;
 
     // Security block stores secured data
