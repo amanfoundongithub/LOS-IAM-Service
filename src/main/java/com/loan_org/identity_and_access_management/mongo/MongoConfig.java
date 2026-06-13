c9f78f289b39d8dcefaf0defc08c49536c6b6494
Aman Raj <billygates124356@gmail.com>
2026-06-13 20:21:41 +0530

    updated

--- src/main/java/com/loan_org/identity_and_access_management/mongo/MongoConfig.java
+++ src/main/java/com/loan_org/identity_and_access_management/mongo/MongoConfig.java
@@ -1,4 +1,4 @@
-package com.loan_org.identity_and_access_management.infra.config;
+package com.loan_org.identity_and_access_management.mongo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
