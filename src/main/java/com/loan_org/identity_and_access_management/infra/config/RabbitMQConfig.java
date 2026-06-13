aa53abfeeede7bd25db492ff4e278149881c5eb8
Aman Raj <billygates124356@gmail.com>
2026-06-13 19:58:32 +0530

    updated, removed unused queue

--- src/main/java/com/loan_org/identity_and_access_management/infra/config/RabbitMQConfig.java
+++ src/main/java/com/loan_org/identity_and_access_management/infra/config/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package com.loan_org.identity_and_access_management.config;
+package com.loan_org.identity_and_access_management.infra.config;
 
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -10,12 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    // Must exactly match the exchange name your Notification Service is listening to
+    // Notification exchange
     public static final String EXCHANGE_NAME = "notification.exchange";
 
-    // Routing keys matching your notification service boundaries
+    // Routing key
     public static final String ROUTING_KEY_HIGH = "notification.routing.high";
-    public static final String ROUTING_KEY_DEFAULT = "notification.routing.default";
 
     @Bean
     public TopicExchange notificationExchange() {
