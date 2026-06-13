dfba193cb44df12292d8d64bbb2cd387cafc4894
Aman Raj <billygates124356@gmail.com>
2026-06-13 19:58:53 +0530

    added cors property to infra

--- src/main/java/com/loan_org/identity_and_access_management/infra/config/CorsProperties.java
+++ src/main/java/com/loan_org/identity_and_access_management/infra/config/CorsProperties.java
@@ -1,4 +1,4 @@
-package com.loan_org.identity_and_access_management.config;
+package com.loan_org.identity_and_access_management.infra.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
