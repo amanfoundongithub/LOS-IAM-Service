6113f89fff426adb776a338b3f771a48f6a333c3
Aman Raj <billygates124356@gmail.com>
2026-06-14 19:19:10 +0530

    updated and added filter ordering

--- src/main/java/com/loan_org/identity_and_access_management/middleware/config/SecurityConfig.java
+++ src/main/java/com/loan_org/identity_and_access_management/middleware/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.loan_org.identity_and_access_management.middleware.config;
 
+import com.loan_org.identity_and_access_management.middleware.filter.MdcHeaderFilter;
+import com.loan_org.identity_and_access_management.middleware.filter.RateLimiterFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,7 +22,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CorsProperties corsProperties;
+    private final CorsProperties    corsProperties;
+    private final RateLimiterFilter rateLimiterFilter;
+    private final MdcHeaderFilter   mdcHeaderFilter;
 
     @Value("${info.base_url}")
     private String authBasePath;
@@ -30,7 +35,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
@@ -39,6 +44,10 @@ public class SecurityConfig {
                         .requestMatchers(authBasePath + "/**").permitAll()
                         .anyRequest().authenticated()
                 );
+
+        // Add filter ordering here
+        http.addFilterBefore(mdcHeaderFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(rateLimiterFilter, MdcHeaderFilter.class);
 
         return http.build();
     }
