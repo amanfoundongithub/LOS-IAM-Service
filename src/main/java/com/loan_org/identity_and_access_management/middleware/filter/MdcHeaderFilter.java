337ab985e68381c80b9c8c80956d5cd21c98cf2a
Aman Raj <billygates124356@gmail.com>
2026-06-14 15:05:58 +0530

    removed unused imports

--- src/main/java/com/loan_org/identity_and_access_management/middleware/filter/MdcHeaderFilter.java
+++ src/main/java/com/loan_org/identity_and_access_management/middleware/filter/MdcHeaderFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
