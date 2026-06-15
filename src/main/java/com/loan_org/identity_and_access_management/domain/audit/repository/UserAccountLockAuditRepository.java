77dd1c773ec1f4fb8d04c7792ba2988d47ef8791
Aman Raj <billygates124356@gmail.com>
2026-06-15 18:32:57 +0530

    added method to find the user account

--- src/main/java/com/loan_org/identity_and_access_management/domain/audit/repository/UserAccountLockAuditRepository.java
+++ src/main/java/com/loan_org/identity_and_access_management/domain/audit/repository/UserAccountLockAuditRepository.java
@@ -4,7 +4,9 @@ import com.loan_org.identity_and_access_management.domain.audit.entity.UserAccou
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserAccountLockAuditRepository extends MongoRepository<UserAccountLockAudit, String> {
-
+    Optional<UserAccountLockAudit> findByLockedByAndLockedAccount(String lockedBy, String lockedAccount);
 }
