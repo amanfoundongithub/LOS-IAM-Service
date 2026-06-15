e09ab74332320aeadc32b37a4ad5eccbc7d9033e
Aman Raj <billygates124356@gmail.com>
2026-06-15 18:07:19 +0530

    repository for auditing

---
+++ src/main/java/com/loan_org/identity_and_access_management/domain/audit/repository/UserAccountLockAuditRepository.java
@@ -0,0 +1,10 @@
+package com.loan_org.identity_and_access_management.domain.audit.repository;
+
+import com.loan_org.identity_and_access_management.domain.audit.entity.UserAccountLockAudit;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountLockAuditRepository extends MongoRepository<UserAccountLockAudit, String> {
+
+}
