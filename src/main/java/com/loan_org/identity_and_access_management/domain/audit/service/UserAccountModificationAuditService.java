bae37b3491f739612d1a707e7cc895fb0a7bcd84
Aman Raj <billygates124356@gmail.com>
2026-06-15 19:17:06 +0530

    refactored code

--- src/main/java/com/loan_org/identity_and_access_management/domain/audit/service/UserAccountModificationAuditService.java
+++ src/main/java/com/loan_org/identity_and_access_management/domain/audit/service/UserAccountModificationAuditService.java
@@ -3,5 +3,5 @@ package com.loan_org.identity_and_access_management.domain.audit.service;
 import com.loan_org.identity_and_access_management.domain.audit.entity.UserAccountModificationAuditDocument;
 
 public interface UserAccountModificationAuditService {
-    void                                 addLog(String affectedUser,UserAccountModificationAuditDocument.ModificationListEntity logEntry);
+    void addLog(String affectedUser,UserAccountModificationAuditDocument.ModificationListEntity logEntry);
 }
